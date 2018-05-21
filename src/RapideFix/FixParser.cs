f5d05df8d6f5ce4ca13d12fec962bc437aad0121
bmolnar2 <molnarbence2@gmail.com>
2018-05-21 21:11:18 +0200

    Providing an example of parsing properties of Order with primitive types.
    FixStringEnumerator: Provides an enumerator over a FIX string. On each enumeration it will return the next element of fix string, separated with vertical bar.
    FixStringEnumerable: Provides an enumerable over a FIX string using FixStringEnumerator.
    ReadonlyMemoryExtensions: Extension methods for ReadonlyMemory<char> representing a FIX element to get FIX tag and value of the element.
    
    Added implementation of FIX string parser in FixParser. It will get the attributes from the type of TMessage type parameter, iterates through the FIX elements with FixStringEnumerable and for each element, if the element's tag had an attribute in the type of TMessage, it will set the value of the element on the TMessage instance, then return the instance.
    
    Added editorconfig.

--- src/RapideFix/FixParser.cs
+++ src/RapideFix/FixParser.cs
@@ -1,20 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Buffers.Text;
+using RapideFix.StringProcessing;
+using RapideFix.Attributes;
 
 namespace RapideFix
 {
   public class FixParser
   {
     private readonly FixParserSettings _settings;
-
+    
     public FixParser(FixParserSettings settings)
     {
       _settings = settings;
     }
 
-    public TMessage Parse<TMessage>(string data)
+    public TMessage Parse<TMessage>(string data) where TMessage : new()
     {
-      throw new NotImplementedException();
+      TMessage message = new TMessage();
+      Dictionary<int, PropertyInfo> tagFixTagAttributeMap = GetPropertyFixTagAttributes(typeof(TMessage));
+
+      foreach (ReadOnlyMemory<char> item in new FixStringEnumerable(data))
+      {
+        (ReadOnlyMemory<char> fixTagChars, ReadOnlyMemory<char> value) = item.GetTagAndValue();
+        int fixTag = int.Parse(fixTagChars.Span);
+
+        if (tagFixTagAttributeMap.TryGetValue(fixTag, out var propertyInfo))
+        {
+          propertyInfo.SetValue(message, Parse(propertyInfo.PropertyType, value));
+        }
+      }
+
+      return message;
     }
 
     public TMessage Parse<TMessage>(byte[] data)
@@ -27,5 +47,47 @@ namespace RapideFix
       throw new NotImplementedException();
     }
 
+    private ReadOnlySpan<char> GetNextElement(int start, string str)
+    {
+      return str.AsSpan(start, str.IndexOf(Constant.VerticalBar, start)-start);
+    }
+
+    #region Private Methods
+
+    private ReadOnlySpan<char> GetValue(ReadOnlySpan<char> element)
+    {
+      int index = element.IndexOf(Constant.Equal) + 1;
+      return element.Slice(index, element.Length - index);
+    }
+
+    private Dictionary<int, PropertyInfo> GetPropertyFixTagAttributes(Type type)
+    {
+      Dictionary<int, PropertyInfo> fixTagPropertyInfoMap = new Dictionary<int, PropertyInfo>();
+      PropertyInfo[] properties = type.GetProperties();
+
+      foreach (PropertyInfo propertyInfo in properties)
+      {
+        FixTagAttribute fixTagAttribute = (FixTagAttribute)propertyInfo.GetCustomAttribute(typeof(FixTagAttribute), false);
+        fixTagPropertyInfoMap.Add(fixTagAttribute.Tag, propertyInfo);
+      }
+
+      return fixTagPropertyInfoMap;
+    }
+
+    private object Parse(Type type, ReadOnlyMemory<char> value)
+    {
+      switch (type.Name)
+      {
+        case "Int32":
+          return int.Parse(value.Span);
+        case "String":
+          return value.Span.ToString();
+        case "Double":
+          return double.Parse(value.ToString()); //Span has issues with double parsing right now
+      }
+      return null;
+    }
+
+    #endregion
   }
 }
---
+++ src/RapideFix/StringProcessing/FixStringEnumerator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RapideFix.StringProcessing
+{
+  public sealed class FixStringEnumerator : IEnumerator<ReadOnlyMemory<char>>
+  {
+    #region Declarations
+
+    private readonly string _fixString;
+
+    private int _index = 0;
+
+    #endregion
+
+    #region Constructors
+
+    public FixStringEnumerator(string fixString)
+    {
+      _fixString = fixString ?? throw new ArgumentNullException(nameof(fixString));
+    }
+
+    #endregion
+
+    #region IEnumerator Properties
+
+    public ReadOnlyMemory<char> Current { get; private set; }
+
+    object IEnumerator.Current => Current;
+
+    #endregion
+
+    #region IEnumerator Methods
+
+    public void Dispose() { }
+
+    public bool MoveNext()
+    {
+      if (_index < _fixString.Length)
+      {
+        int nextSeparator = _fixString.IndexOf(Constant.VerticalBar, _index);
+        if (0 < nextSeparator)
+        {
+          Current = _fixString.AsMemory(_index, nextSeparator - _index);
+          _index = nextSeparator + 1;
+          return true;
+        }
+      }
+
+      return false;
+    }
+
+    public void Reset()
+    {
+      _index = 0;
+      Current = null;
+    }
+
+    #endregion
+  }
+}
